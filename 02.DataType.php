cae8d0e38e6aa78a3e128c21db5de836ecccee14
singsong19 <69883408+singsong19@users.noreply.github.com>
2021-01-11 22:00:23 +0900

    Add files via upload

---
+++ 02.DataType.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <body>
+    <!-- number & Arithmetic Operator -->
+    <?php
+    echo 1; 
+    print(1);
+    echo 1.1;
+    echo 1+1;
+    ?> <?php
+    echo 4/2; 
+    ?> <?php
+    echo 2*2; 
+    ?>
+    </body>
+</html>
